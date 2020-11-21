820c80ae30ed4dab6642ff79466a3e5900763702
jsandas <jsandas82@gmail.com>
2020-11-21 11:36:27 -0700

    increase timeout to 60 seconds

--- test-env/test-env.sh
+++ test-env/test-env.sh
@@ -21,11 +21,13 @@ start () {
     echo ""
     echo "Waiting for admin.password to be generated"
     _dur=0
+    _timeout=60
     until docker exec nexus3 bash -c 'test -f /nexus-data/admin.password'
     do
-        if [ $_dur -gt 30 ]; then
-            echo "Couldn't get admin password"
+        if [ $_dur -gt $_timeout ]; then
+            echo "Couldn't get admin password after $_timeout seconds"
             echo "Please check containers logs for details"
+            echo " docker logs nexus3"
             exit 1
         fi
         _dur=$((_dur+1))
