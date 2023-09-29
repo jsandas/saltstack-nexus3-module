d17fbff148de45677311dee46ca09da6ab11f81d
jsandas <jsandas@users.noreply.github.com>
2023-09-28 21:24:17 -0600

    updated blobstores integration test

--- test-env/tests/test_blobstores.py
+++ test-env/tests/test_blobstores.py
@@ -1,74 +1,145 @@
 #!/usr/bin/env python3
 
+import pprint 
+import sys
+
 import salt.client
 
 client = salt.client.LocalClient()
 
-def test_create_blobstore():
-    ret = client.cmd('test.minion', 'nexus3_blobstores.create', ['name=test1'])
+test_data ={
+    'test_blobstore': {
+        'inputs': [],
+        'results': {
+            'type': 'File'
+        },
+    },
+    'test_s3_blobstore': {
+        'inputs': [
+            'store_type=s3',
+            's3_bucket=nexus3',
+            's3_accessKeyId=AKIAIOSFODNN7EXAMPLE',
+            's3_secretAccessKey=wJalrXUtnFEMIK7MDENGbPxRfiCYEXAMPLEKEY',
+            's3_endpoint=http://minio:9000',
+            's3_forcePathStyle=True'
+        ],
+        'results': {
+            'type': 'S3'
+        },
+    },
+}
+
+
+def main():
+    blobstores = []
+
+    for name, values in test_data.items():
+        # print(" Creating blobstore {}".format(name))
+        try:
+            create_blobstore(name, values)
+            blobstores.append(name)
+        except:
+            print(" Failed creating blobstore {}".format(name))
+
+    list_blobstores(len(blobstores))
+
+    for name in blobstores:
+        # print(" Deleting blobstore {}".format(name))
+        delete_blobstore(name)
+
+    # print(" Running blobstore states")
+    blobstores_state()
+
+
+def create_blobstore(name, values):
+    list1 = [name]
+    args = list1 + values['inputs']
+
+    ret = client.cmd('test.minion', 'nexus3_blobstores.create', args)
     # print(ret)
     assert ret['test.minion']['blobstore'] != {},'blobstore is empty'
-    assert ret['test.minion']['blobstore']['name'] == 'test1','blobstore test1 not created'
-    assert ret['test.minion']['blobstore']['type'] == 'File','wrong store type found'
+    assert ret['test.minion']['blobstore']['name'] == name,'blobstore {} not created'.format(name)
+    assert ret['test.minion']['blobstore']['type'] == values['results']['type'],'wrong store type {} found'.format(values['results']['type'])
 
-    ret = client.cmd('test.minion', 'nexus3_blobstores.create', ['name=test2', 'store_type=s3', 
-                        's3_bucket=nexus3', 's3_accessKeyId=AKIAIOSFODNN7EXAMPLE', 
-                        's3_secretAccessKey=wJalrXUtnFEMIK7MDENGbPxRfiCYEXAMPLEKEY', 
-                        's3_endpoint=http://minio:9000', 's3_forcePathStyle=True'])
+
+def delete_blobstore(blobstore):
+    ret = client.cmd('test.minion', 'nexus3_blobstores.delete', ['name={}'.format(blobstore)])
     # print(ret)
-    assert ret['test.minion']['blobstore'] != {},'blobstore is empty'
-    assert ret['test.minion']['blobstore']['name'] == 'test2','blobstore test2 not created'
-    assert ret['test.minion']['blobstore']['type'] == 'S3','wrong store type found'
+    assert ret['test.minion']['comment'] == 'Deleted blobstore "{}"'.format(blobstore),'blobstore {} not deleted'.format(blobstore)
 
 
-def test_update_blobstore():
-    ret = client.cmd('test.minion', 'nexus3_blobstores.update', ['name=test1','quota_type=spaceRemainingQuota',
-                                                                    'quota_limit=5000000'])
-    # print(ret)
-    assert ret['test.minion']['blobstore'] != {},'data is empty'
-    assert ret['test.minion']['blobstore']['name'] == 'test1','blobstore test1 not found'
-    assert ret['test.minion']['blobstore']['softQuota']['type'] == 'spaceRemainingQuota','wrong quota type found'
-    assert ret['test.minion']['blobstore']['softQuota']['limit'] == 5000000,'wrong quota limit found'
-
-
-def test_describe_blobstore():
-    ret = client.cmd('test.minion', 'nexus3_blobstores.describe', ['name=test2'])
-    # print(ret)
-    assert ret['test.minion']['blobstore'] != {},'data is empty'
-    assert ret['test.minion']['blobstore']['name'] == 'test2','blobstore test1 not found'
-    assert ret['test.minion']['blobstore']['type'] == 'S3','wrong store type found'
-    assert ret['test.minion']['blobstore']['bucketConfiguration']['bucket']['name'] == 'nexus3','wrong bucket name found'
-
-def test_list_blobstores():
+def list_blobstores(count):
     ret = client.cmd('test.minion', 'nexus3_blobstores.list_all')
     # print(ret)
     assert ret['test.minion']['blobstores'] != {},'data is empty'
-    # this test is brittle as it assumes no other blobstores exists
-    # in the nexus instance being tested
-    # there should be three blobstores: default, test1, and test2
-    # assert len(ret['test.minion']['blobstore']) == 3,'incorrect blobstore count'
 
 
-def test_delete_blobstore():
-    ret = client.cmd('test.minion', 'nexus3_blobstores.delete', ['name=test1'])
-    # print(ret)
-    assert ret['test.minion']['comment'] == 'Deleted blobstore "test1"','blobstore test1 not deleted'
+def blobstores_state():
+    pillar = {
+        "nexus": {
+            "blobstores": {
+                "apt": [],
+                "docker": [],
+                "maven": [
+                    {"quota_type": "spaceRemainingQuota"},
+                    {"quota_limit": 1000000000}
+                ],
+                "s3blobstore": [
+                    {"store_type": "s3"},
+                    {"s3_bucket": "nexus3"},
+                    {"s3_accessKeyId": "AKIAIOSFODNN7EXAMPLE"},
+                    {"s3_secretAccessKey": "wJalrXUtnFEMIK7MDENGbPxRfiCYEXAMPLEKEY"},
+                    {"s3_endpoint": "http://minio:9000"},
+                    {"s3_forcePathStyle": True}
+                ],
+                "yum": []
+            },
+        }
+    }
 
-    ret = client.cmd('test.minion', 'nexus3_blobstores.delete', ['name=test2'])
-    # print(ret)
-    assert ret['test.minion']['comment'] == 'Deleted blobstore "test2"','blobstore test2 not deleted'
+    pillar_update = {
+        "nexus": {
+            "blobstores": {
+                "maven": [
+                    {"quota_type": "spaceRemainingQuota"},
+                    {"quota_limit": 2000000000}
+                ],
+            },
+        }
+    }
+    # pp = pprint.PrettyPrinter(indent=2)
+    # pp.pprint(pillar)
+    ret = client.cmd('test.minion', 'state.apply', ['nexus3.blobstores', f'pillar={pillar}'])
+    # pp.pprint(ret['test.minion'])
+    validate_return(ret)
+
+    # pp.pprint(pillar_update)
+    ret = client.cmd('test.minion', 'state.apply', ['nexus3.blobstores', 'pillar={}'.format(pillar_update), 'test=True'])
+    # pp.pprint(ret['test.minion'])
+
+    # clean up
+    for blobstore in pillar['nexus']['blobstores']:
+        delete_blobstore(blobstore)
 
 
-# clean the slate
-client.cmd('test.minion', 'nexus3_blobstores.delete', ['name=test1'])
-client.cmd('test.minion', 'nexus3_blobstores.delete', ['name=test2'])
+def validate_return(ret):
+    pp = pprint.PrettyPrinter(indent=2)
 
-test_create_blobstore()
+    try:
+        for key, value in ret['test.minion'].items():
+            if value['result']:
+                continue
 
-test_update_blobstore()
+            print("state failure occurred")
+            print("state name: '{}'\nreturn value:").format(key)
+            pp.pprint(value)
+            sys.exit()
 
-test_describe_blobstore()
+        # pp.pprint(ret)
+    except Exception as e:
+        print(e)
+        pp.pprint(ret['test.minion'])
 
-test_list_blobstores()
 
-test_delete_blobstore()
+if __name__ == "__main__":
+    main()
